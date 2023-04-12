7ebd55a0b3e6b01c60afbb6333380a0304099a6f
Lokeshsharma1703 <lokeshsharma17032003@gmail.com>
2023-04-12 14:10:37 +0530

    initial commit

---
+++ react-demo/src/components/Person.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './person.css'
+
+const Person = () => {
+    return (
+        <div className='person'>
+            <p>Name : ABC</p>
+            <p>Age : 26</p>
+            <p>Location : Delhi</p>
+        </div>
+    )
+}
+
+export default Person
