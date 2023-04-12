be49816d1862580dc637cf2fc0ae66b6ca4b989e
Lokeshsharma1703 <lokeshsharma17032003@gmail.com>
2023-04-12 14:40:31 +0530

    initial commit

--- react-demo/src/components/Person.js
+++ react-demo/src/components/Person.js
@@ -2,11 +2,22 @@ import React from 'react'
 import './person.css'
 
 const Person = () => {
+
+    let luckyNumber = 7;
+    let randomNumber = Math.floor(Math.random() * 10);
+
     return (
-        <div className='person'>
-            <p>Name : ABC</p>
-            <p>Age : 26</p>
-            <p>Location : Delhi</p>
+        <div className='person' style={{ color: 'green', fontWeight: 'bold' }}>
+            {
+                (randomNumber === luckyNumber) ?
+                    <div>
+                        <p>Name : ABC</p>
+                        <p>Age : 26</p>
+                        <p>Location : Delhi</p>
+
+                        <p>My number is : {randomNumber}</p>
+                    </div> : <p>You didn't win 🥲</p>
+            }
         </div>
     )
 }
