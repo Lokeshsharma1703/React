84ee28ff2f4ac248df57681532a1903d2bbf9bd3
Lokeshsharma1703 <lokeshsharma17032003@gmail.com>
2023-04-19 15:03:26 +0530

    custom hook

--- react-hook/src/App.js
+++ react-hook/src/App.js
@@ -1,9 +1,27 @@
+import { useState } from 'react';
 import './App.css';
+import Section from './componenets/Section';
+import useToggle from './componenets/hooks/useToggle.js';
 
 function App() {
+
+  const [smileyFace, setSmileyFace] = useState(true)
+
+  // const [section, setSection] = useState(true)
+
+  const [section, setSection] = useToggle();
+
+
+
   return (
     <div className="App">
-      <h1>React-Custom-Hooks</h1>
+
+      {section && <Section />}
+
+      <button onClick={() => { setSection() }} style={{ height: '40px', width: '120px', backgroundColor: 'skyblue', fontSize: '18px', borderRadius: '10px', marginTop: '50px' }}>Read {section ? 'More' : 'Less'}</button>
+
+
+      <h1 onClick={() => { setSmileyFace(!smileyFace) }}>{(smileyFace) ? "😃" : "🗿"}</h1>
     </div>
   );
 }
---
+++ react-hook/src/componenets/Section.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Section = () => {
+    return (
+        <div>
+            <p style={{ width: "70vw", backgroundColor: 'aqua', border: '2px solid black', margin: '50px auto', borderRadius: '15px', padding: '1rem' }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae nihil quisquam neque veritatis maiores! Laudantium aliquid fugit reiciendis ducimus ratione neque cupiditate tenetur repudiandae quae quo numquam optio quidem at, consectetur quasi. Perspiciatis officiis eum doloribus esse officia hic quisquam praesentium provident laboriosam nobis, nam earum repellat assumenda libero error voluptates non ea, aspernatur accusantium. Maiores blanditiis sed nobis? Amet illum enim architecto voluptatem eum facilis, quod minima temporibus accusantium. Impedit beatae itaque eligendi magni eius ut ducimus. Repellat tenetur non architecto modi eveniet possimus aliquid, error expedita itaque fugit temporibus numquam deserunt excepturi odio vero vitae quisquam ab? A.</p>
+        </div>
+    )
+}
+
+export default Section
